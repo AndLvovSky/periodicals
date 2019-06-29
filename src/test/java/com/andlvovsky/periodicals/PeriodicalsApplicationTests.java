9018a1c20e76552644975c36cc78e532a0183c3a
AndLvovSky <viktorilvovskyi@ukr.net>
2019-06-29 18:12:15 +0300

    Check controllers and repositories creation in PeriodicalsApplicationTests

--- src/test/java/com/andlvovsky/periodicals/PeriodicalsApplicationTests.java
+++ src/test/java/com/andlvovsky/periodicals/PeriodicalsApplicationTests.java
@@ -1,16 +1,34 @@
 package com.andlvovsky.periodicals;
 
+import com.andlvovsky.periodicals.controller.PublicationController;
+import com.andlvovsky.periodicals.controller.PublicationControllerAdvice;
+import com.andlvovsky.periodicals.repository.PublicationRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PeriodicalsApplicationTests {
 
+    @Autowired
+    private PublicationController publicationController;
+
+    @Autowired
+    private PublicationControllerAdvice publicationControllerAdvice;
+
+    @Autowired
+    private PublicationRepository publicationRepository;
+
     @Test
     public void contextLoads() {
+        assertNotNull(publicationController);
+        assertNotNull(publicationControllerAdvice);
+        assertNotNull(publicationRepository);
     }
 
 }
