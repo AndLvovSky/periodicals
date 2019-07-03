33d6efb345fcdaa9cccef1c938f68bb5489b0c8b
AndLvovSky <viktorilvovskyi@ukr.net>
2019-07-03 13:08:06 +0300

    Use WebMvcTest annotation instead of SpringBootTest

--- src/test/java/com/andlvovsky/periodicals/PublicationControllerTests.java
+++ src/test/java/com/andlvovsky/periodicals/PublicationControllerTests.java
@@ -1,7 +1,8 @@
 package com.andlvovsky.periodicals;
 
+import com.andlvovsky.periodicals.exception.PublicationNotFoundException;
 import com.andlvovsky.periodicals.model.Publication;
-import com.andlvovsky.periodicals.repository.PublicationRepository;
+import com.andlvovsky.periodicals.service.PublicationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -9,7 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,7 +18,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.*;
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class PublicationControllerTests {
@@ -35,7 +35,7 @@ public class PublicationControllerTests {
     private MockMvc mvc;
 
     @MockBean
-    private PublicationRepository repository;
+    private PublicationService service;
 
     Publication[] publications = {
             new Publication("The Guardian", 7, 10., "-"),
@@ -47,59 +47,65 @@ public class PublicationControllerTests {
 
     @Before
     public void defineRepositoryBehavior() {
-        when(repository.findById(any(Long.class))).thenReturn(Optional.empty());
-        when(repository.findById(1L)).thenReturn(Optional.of(publications[0]));
-        when(repository.findById(2L)).thenReturn(Optional.of(publications[1]));
-        when(repository.findById(3L)).thenReturn(Optional.of(publications[2]));
-        when(repository.findAll()).thenReturn(Arrays.asList(publications[0], publications[1], publications[2]));
+        when(service.getOne(1L)).thenReturn(publications[0]);
+        when(service.getOne(2L)).thenReturn(publications[1]);
+        when(service.getOne(3L)).thenReturn(publications[2]);
+        when(service.getAll()).thenReturn(Arrays.asList(publications[0], publications[1], publications[2]));
     }
 
     @Test
-    public void testGetPublication() throws Exception {
+    public void testGetOne() throws Exception {
         mvc.perform(get("/publications/1")).andExpect(status().isOk()).andDo(print())
             .andExpect(jsonPath("$.name").value("The Guardian"));
     }
 
     @Test
-    public void testGetPublicationFail() throws Exception {
-        mvc.perform(get("/publications/99")).andDo(print())
+    public void testGetOneFail() throws Exception {
+        when(service.getOne(99L)).thenThrow(new PublicationNotFoundException(99L));
+        mvc.perform(get("/publications/99")).andExpect(status().isNotFound()).andDo(print())
                 .andExpect(content().string(allOf(Matchers.containsString("cannot find"),
                         Matchers.containsString("99"))));
     }
 
     @Test
-    public void testGetAllPublications() throws Exception {
+    public void testGetAll() throws Exception {
         mvc.perform(get("/publications/")).andDo(print())
                 .andExpect(jsonPath("$.length()").value(3))
                 .andExpect(jsonPath("$[2].frequency").value(14));
     }
 
     @Test
-    public void testAddPublication() throws Exception {
+    public void testAdd() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String publicationJson = mapper.writeValueAsString(publications[3]);
         mvc.perform(post("/publications/").content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isCreated());
-        verify(repository).save(publications[3]);
-        verifyNoMoreInteractions(repository);
+        verify(service).add(publications[3]);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
-    public void testReplacePublication() throws Exception {
-        when(repository.save(publications[4])).thenReturn(publications[4]);
+    public void testReplace() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String publicationJson = mapper.writeValueAsString(publications[4]);
         mvc.perform(put("/publications/2").content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isNoContent());
-        verify(repository).findById(2L);
-        verify(repository).save(publications[4]);
-        verifyNoMoreInteractions(repository);
+        verify(service).replace(2L, publications[4]);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
-    public void testDeletePublication() throws Exception {
+    public void testDelete() throws Exception {
         mvc.perform(delete("/publications/3")).andExpect(status().isNoContent());
-        verify(repository).deleteById(3L);
+        verify(service).delete(3L);
+    }
+
+    @Test
+    public void testDeleteFail() throws Exception {
+        doThrow(new PublicationNotFoundException(88L)).when(service).delete(88L);
+        mvc.perform(delete("/publications/88")).andExpect(status().isNotFound()).andDo(print())
+                .andExpect(content().string(allOf(Matchers.containsString("cannot find"),
+                        Matchers.containsString("88"))));
     }
 
 }
