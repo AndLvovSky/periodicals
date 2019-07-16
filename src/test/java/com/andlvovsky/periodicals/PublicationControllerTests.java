78d9dbd95f1ed481b62555e325201cc4020c07f1
AndLvovSky <viktorilvovskyi@ukr.net>
2019-07-16 19:48:51 +0300

    Improve PublicationControllerTests

--- src/test/java/com/andlvovsky/periodicals/PublicationControllerTests.java
+++ src/test/java/com/andlvovsky/periodicals/PublicationControllerTests.java
@@ -8,6 +8,7 @@ import com.andlvovsky.periodicals.service.PublicationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mapstruct.factory.Mappers;
@@ -40,16 +41,27 @@ public class PublicationControllerTests {
     @MockBean
     private PublicationService service;
 
-    private PublicationMapper publicationDtoMapper = Mappers.getMapper(PublicationMapper.class);
+    private ObjectMapper jsonMapper = new ObjectMapper();
 
-    private Publication[] publications = {
-            new Publication("The Guardian", 7, 10., "-"),
-            new Publication("Daily Mail", 1, 5.5, "-"),
-            new Publication("The Washington Post", 14, 17., "-"),
-            new Publication("The Sun", 30, 20., "-"),
-            new Publication("The Wall Street Journal", 7, 15., "-"),
-            new Publication("The New Yorker", -5, 10., "-")
-    };
+    private static Publication[] publications;
+
+    private static PublicationDto[] publicationDtos;
+
+    @BeforeClass
+    public static void initializePublicationsAndCorrespondingDtos() {
+        publications = new Publication[6];
+        publications[0] = new Publication("The Guardian", 7, 10., "-");
+        publications[1] = new Publication("Daily Mail", 1, 5.5, "-");
+        publications[2] = new Publication("The Washington Post", 14, 17., "-");
+        publications[3] = new Publication("The Sun", 30, 20., "-");
+        publications[4] = new Publication("The Wall Street Journal", 7, 15., "-");
+        publications[5] = new Publication("The New Yorker", -5, 10., "-");
+        publicationDtos = new PublicationDto[publications.length];
+        PublicationMapper publicationDtoMapper = Mappers.getMapper(PublicationMapper.class);
+        for (int i = 0; i < publications.length; i++) {
+            publicationDtos[i] = publicationDtoMapper.toDto(publications[i]);
+        }
+    }
 
     @Before
     public void defineRepositoryBehavior() {
@@ -57,35 +69,35 @@ public class PublicationControllerTests {
         when(service.getOne(2L)).thenReturn(publications[1]);
         when(service.getOne(3L)).thenReturn(publications[2]);
         when(service.getAll()).thenReturn(Arrays.asList(publications[0], publications[1], publications[2]));
+        when(service.getOne(99L)).thenThrow(new PublicationNotFoundException(99L));
+        doThrow(new PublicationNotFoundException(88L)).when(service).delete(88L);
     }
+
 
     @Test
     public void getsOne() throws Exception {
-        mvc.perform(get("/publications/1")).andExpect(status().isOk()).andDo(print())
-                .andExpect(jsonPath("$.name").value("The Guardian"));
+        mvc.perform(get(url("1"))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("The Guardian")).andDo(print());
     }
 
     @Test
     public void getsOneFail() throws Exception {
-        when(service.getOne(99L)).thenThrow(new PublicationNotFoundException(99L));
-        mvc.perform(get("/publications/99")).andExpect(status().isNotFound()).andDo(print())
+        mvc.perform(get(url("99"))).andExpect(status().isNotFound())
                 .andExpect(content().string(allOf(Matchers.containsString("cannot find"),
-                        Matchers.containsString("99"))));
+                        Matchers.containsString("99")))).andDo(print());
     }
 
     @Test
     public void getsAll() throws Exception {
-        mvc.perform(get("/publications/")).andDo(print())
+        mvc.perform(get(url("")))
                 .andExpect(jsonPath("$.length()").value(3))
-                .andExpect(jsonPath("$[2].period").value(14));
+                .andExpect(jsonPath("$[2].period").value(14)).andDo(print());
     }
 
     @Test
     public void adds() throws Exception {
-        PublicationDto publicationDto = publicationDtoMapper.toDto(publications[3]);
-        ObjectMapper jsonMapper = new ObjectMapper();
-        String publicationJson = jsonMapper.writeValueAsString(publicationDto);
-        mvc.perform(post("/publications/").content(publicationJson)
+        String publicationJson = jsonMapper.writeValueAsString(publicationDtos[3]);
+        mvc.perform(post(url("")).content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isCreated());
         verify(service).add(publications[3]);
         verifyNoMoreInteractions(service);
@@ -93,10 +105,8 @@ public class PublicationControllerTests {
 
     @Test
     public void additionFails() throws Exception {
-        PublicationDto publicationDto = publicationDtoMapper.toDto(publications[5]);
-        ObjectMapper jsonMapper = new ObjectMapper();
-        String publicationJson = jsonMapper.writeValueAsString(publicationDto);
-        mvc.perform(post("/publications/").content(publicationJson)
+        String publicationJson = jsonMapper.writeValueAsString(publicationDtos[5]);
+        mvc.perform(post(url("")).content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isBadRequest()).andDo(print());
         verifyNoMoreInteractions(service);
@@ -104,10 +114,8 @@ public class PublicationControllerTests {
 
     @Test
     public void replaces() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        PublicationDto publicationDto = publicationDtoMapper.toDto(publications[4]);
-        String publicationJson = mapper.writeValueAsString(publicationDto);
-        mvc.perform(put("/publications/2").content(publicationJson)
+        String publicationJson = jsonMapper.writeValueAsString(publicationDtos[4]);
+        mvc.perform(put(url("2")).content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isOk());
         verify(service).replace(2L, publications[4]);
         verifyNoMoreInteractions(service);
@@ -115,10 +123,8 @@ public class PublicationControllerTests {
 
     @Test
     public void replacementFails() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        PublicationDto publicationDto = publicationDtoMapper.toDto(publications[5]);
-        String publicationJson = mapper.writeValueAsString(publicationDto);
-        mvc.perform(put("/publications/2").content(publicationJson)
+        String publicationJson = jsonMapper.writeValueAsString(publicationDtos[5]);
+        mvc.perform(put(url("2")).content(publicationJson)
                 .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isBadRequest()).andDo(print());
         verifyNoMoreInteractions(service);
@@ -126,16 +132,19 @@ public class PublicationControllerTests {
 
     @Test
     public void deletes() throws Exception {
-        mvc.perform(delete("/publications/3")).andExpect(status().isNoContent());
+        mvc.perform(delete(url("3"))).andExpect(status().isNoContent());
         verify(service).delete(3L);
     }
 
     @Test
     public void deletionFails() throws Exception {
-        doThrow(new PublicationNotFoundException(88L)).when(service).delete(88L);
-        mvc.perform(delete("/publications/88")).andExpect(status().isNotFound()).andDo(print())
+        mvc.perform(delete(url("88"))).andExpect(status().isNotFound())
                 .andExpect(content().string(allOf(Matchers.containsString("cannot find"),
-                        Matchers.containsString("88"))));
+                        Matchers.containsString("88")))).andDo(print());
+    }
+
+    private String url(String suffix) {
+        return "/publications/" + suffix;
     }
 
 }
