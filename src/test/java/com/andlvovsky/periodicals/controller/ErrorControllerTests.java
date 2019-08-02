5247cd6714cc6886bf5cb9b3494b95c02815e232
AndLvovSky <viktorilvovskyi@ukr.net>
2019-08-02 11:16:49 +0300

    Allow unauthenticated users to visit error page

--- src/test/java/com/andlvovsky/periodicals/controller/ErrorControllerTests.java
+++ src/test/java/com/andlvovsky/periodicals/controller/ErrorControllerTests.java
@@ -1,16 +1,10 @@
 package com.andlvovsky.periodicals.controller;
 
-import com.andlvovsky.periodicals.service.OrderService;
-import com.andlvovsky.periodicals.service.PublicationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.RequestDispatcher;
 
@@ -22,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest
 @AutoConfigureMockMvc
-@WithMockUser
 public class ErrorControllerTests extends ControllerTests {
 
     private static final String URL = "/error";
