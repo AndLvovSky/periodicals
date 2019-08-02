484eb5edc21322a1f5881a3065ad973a14f96a0e
AndLvovSky <viktorilvovskyi@ukr.net>
2019-08-02 16:33:26 +0300

    Sort publications by id, modify edit page

--- src/main/java/com/andlvovsky/periodicals/repository/PublicationRepository.java
+++ src/main/java/com/andlvovsky/periodicals/repository/PublicationRepository.java
@@ -3,6 +3,10 @@ package com.andlvovsky.periodicals.repository;
 import com.andlvovsky.periodicals.model.publication.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PublicationRepository extends JpaRepository<Publication, Long> {
+
+    List<Publication> findAllByOrderById();
 
 }
